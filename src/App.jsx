cc4e2c419d8c96cecbaa60ef20ae47247621dd42
Purvi-spec <purvisingh1482@gmail.com>
2024-07-01 16:31:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Portfolio from "./Components/Portfolio";
+import Experiance from "./Components/Experiance";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import {Toaster} from "react-hot-toast";
+function App() {
+  return (
+    <>
+     <div>
+     <Navbar />
+        <Home />
+        <About />
+        <Portfolio />
+        <Experiance />
+        <Contact />
+        <Footer />
+     </div>
+     <Toaster />
+    </>
+  );
+}
+
+export default App;
